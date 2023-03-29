26baad8a68a39b6bb841990a2005d6743ae6be4a
luciferpeter88git config user.email kaszap.nagy.peter@citromail.hu git config user.name luciferpeter88git config --global user.email kaszap.nagy.peter@citromail.hu <luciferpeter88>
2023-03-29 04:39:01 -0400

    -

--- front_end/src/components/UI/Home/Card.jsx
+++ front_end/src/components/UI/Home/Card.jsx
@@ -3,32 +3,32 @@ import pic from "../../../assets/images/Home/homeCardPic.png";
 
 function Card() {
   return (
-    <div class="max-w-sm  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center py-3 drop-shadow-lg">
+    <div className="max-w-sm  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center py-3 drop-shadow-lg">
       <div>
-        <img class="rounded-t-lg" src={pic} alt="" />
+        <img className="rounded-t-lg" src={pic} alt="" />
       </div>
-      <div class="p-5">
+      <div className="p-5">
         <div>
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             The Title Of The Events
           </h5>
           <p>January 25, 2021</p>
         </div>
-        <p class="mb-3 font-normal mt-3 text-gray-700 dark:text-gray-400">
+        <p className="mb-3 font-normal mt-3 text-gray-700 dark:text-gray-400">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget nunc,
         </p>
-        <div class="inline-flex items-center px-3 py-2 text-sm font text-center text-white   bg-green-900 h-10 w-32 rounded font-medium  pointer">
+        <div className="inline-flex items-center px-3 py-2 text-sm font text-center text-white   bg-green-900 h-10 w-32 rounded font-medium  pointer">
           Read more
           <svg
             aria-hidden="true"
-            class="w-4 h-4 ml-2 -mr-1"
+            className="w-4 h-4 ml-2 -mr-1"
             fill="currentColor"
             viewBox="0 0 20 20"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </div>
--- front_end/src/components/UI/Home/DetailsCard.jsx
+++ front_end/src/components/UI/Home/DetailsCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DetailsCard({ icon, description, color }) {
   return (
-    <div class="h-60 w-60  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center py-3 drop-shadow-lg gap-5">
+    <div className="h-60 w-60  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center items-center py-3 drop-shadow-lg gap-5">
       <div
         className={`${color} rounded-full h-24 w-24 flex justify-center items-center text-white text-5xl`}
       >

7722a41bc1dc3549f2844f4304fd8951d3aadf5d
luciferpeter88git config user.email kaszap.nagy.peter@citromail.hu git config user.name luciferpeter88git config --global user.email kaszap.nagy.peter@citromail.hu <luciferpeter88>
2023-05-24 08:23:32 -0400

    .

--- front_end/src/components/UI/dashboard/helper/Overlay.jsx
+++ front_end/src/components/UI/dashboard/helper/Overlay.jsx
@@ -4,7 +4,7 @@ function Overlay() {
   return (
     <div className=" absolute top-0 left-0 bg-black h-[150vh] opacity-70 w-full z-10 flex justify-center items-center">
       <div className=" text-yellow-300 mb-[50%] text-2xl uppercase">
-        Waiting for approving!
+        Waiting for approval!
       </div>
     </div>
   );
